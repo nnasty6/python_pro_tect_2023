4ff998722467c46616b68301696ed358fe5f8d73
Anastasiia <ger.anastasia.a@gmail.com>
2023-08-11 18:03:02 +0300

    code quality is provided

---
+++ lesson_02/test_quality_hw.py
@@ -0,0 +1,14 @@
+def what_i_am_doing():
+    print("I do not understand what i am doing wrong")
+
+
+def why():
+    pass
+
+
+def pre_commit():
+    print("It does not work, linter and formatters are 'not installed'")
+
+
+def why1():
+    pass
